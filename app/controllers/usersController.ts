b55adf75f1e45c9ca3c299d371ccd41ab9778b45
Castro456 <castrosid456@gmail.com>
2025-02-25 00:23:41 +0530

    Added comments

--- app/controllers/usersController.ts
+++ app/controllers/usersController.ts
@@ -2,6 +2,7 @@ import {Request, Response, NextFunction} from "express";
 import * as userServices from "../services/users.service"
 
 export const getUsers = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  // The function getUsersList is asynchronous, meaning it performs operations that take time (like fetching data from a database). In JavaScript, asynchronous functions return a Promise, which represents the eventual completion (or failure) of that operation.
   try {
     const usersList = await userServices.getUsersList();
     res.json(usersList);
